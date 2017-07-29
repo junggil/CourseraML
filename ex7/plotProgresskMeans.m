ebe6624d732d3067a9bcc01e17403a0968c2f704
이정길/선임연구원/클라우드센터 서비스플랫폼FD 서비스개발팀(jeongkil.lee@lge.com) <jeongkil.lee@lge.com>
2017-07-29 09:57:46 +0900

    add ex8

--- ex7/plotProgresskMeans.m
+++ ex7/plotProgresskMeans.m
@@ -13,7 +13,7 @@ plotDataPoints(X, idx, K);
 % Plot the centroids as black x's
 plot(centroids(:,1), centroids(:,2), 'x', ...
      'MarkerEdgeColor','k', ...
-     'MarkerSize', 10, 'LineWidth', 3);
+     'MarkerSize', 3, 'LineWidth', 2);
 
 % Plot the history of the centroids with lines
 for j=1:size(centroids,1)
